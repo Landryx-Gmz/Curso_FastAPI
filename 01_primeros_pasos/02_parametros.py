cc7afa83f0afc201a4f385909c170fc89b805591
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-08-11 07:03:57 +0200

    refactor parametros

--- 01_primeros_pasos/02_parametros.py
+++ 01_primeros_pasos/02_parametros.py
@@ -7,6 +7,6 @@ async def get_favoritre_book():
     return {"title" : "1984"}
 
 @app.get("/books/{book_id}")
-async def get_book(book_id: str):
+async def get_book(book_id: int):
     return {"book_id": book_id}
 
