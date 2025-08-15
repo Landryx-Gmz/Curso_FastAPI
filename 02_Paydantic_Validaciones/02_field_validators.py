faf6a7eebeb08781f5b6b0e3c6a6c7bd95239dcd
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-08-15 10:53:51 +0200

    fiel validators numero par

---
+++ 02_Paydantic_Validaciones/02_field_validators.py
@@ -0,0 +1,19 @@
+#After: Corre despued de las validaciones/transformaciones de pydantic
+#Before: Corre antes de las validaciones/transformaciones de pydantic
+#Plain: similar a before termina al retornar el valor
+#Before: Flexible antes o despues de las validaciones de pydantic
+
+
+from typing import Annotated
+from pydantic import AfterValidator, BaseModel
+
+# Función para validar un campo
+def es_par(value: int) -> int:
+    if value %2 == 1:
+        raise ValueError(f"{value} no es numero par")
+    return value
+
+#validacion = Annotated[tipehim, AfterValidator(funcion)]
+
+NumeroPar = Annotated[int, AfterValidator(es_par)]
+
