97d6cc4b5abeb8cd0b8531697dce32ac66698c68
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-08-17 10:10:48 +0200

    ejemplo de validaciones(lambda y lista en model1  de fierld validators

--- 02_Paydantic_Validaciones/02_field_validators.py
+++ 02_Paydantic_Validaciones/02_field_validators.py
@@ -7,9 +7,11 @@
 from typing import Annotated
 from pydantic import AfterValidator, BaseModel
 
+# ==============================ANNOTATED==================================
+
 # Función para validar un campo
 def es_par(value: int) -> int:
-    if value %2 == 1:
+    if value % 2 == 1:
         raise ValueError(f"{value} no es numero par")
     return value
 
@@ -19,10 +21,15 @@ NumeroPar = Annotated[int, AfterValidator(es_par)]
 
 class Model1(BaseModel):
     my_number: NumeroPar
-ejemplo: Model1 = Model1(my_number=2)
+#ejemplo: Model1 = Model1(my_number=3)
 
+class Model2(BaseModel):
+    other_number: Annotated[NumeroPar, AfterValidator(lambda v: v+2)]
+# ejemplo2: Model2 = Model2(other_number=4)
+# print(ejemplo2)
 
-# class Model2(BaseModel):
-#     my_number:
-# class Model3(BaseModel):
+class Model3(BaseModel):
+    lista_pares: list[NumeroPar]
+ejemplo3: Model3 = Model3(lista_pares=[2,5,10])
+print(ejemplo3)
 
