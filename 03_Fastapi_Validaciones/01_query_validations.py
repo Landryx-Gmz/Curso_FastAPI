d4ba4dbcf1c0c0a4f6f3a4c54a7df9d92e75e7c4
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-08-18 17:18:45 +0200

    query con metadata tittle y description

--- 03_Fastapi_Validaciones/01_query_validations.py
+++ 03_Fastapi_Validaciones/01_query_validations.py
@@ -8,18 +8,47 @@ app = FastAPI()
 # min_length
 # pattern
 
-#==============Validaciones para Números==================
-# gt (greater than)
-# ge (greater than or equal)
-# lt (less than)
-#le (less than or equal)
+
+
+
 def check_valid_id(id: str):
     if id % 2 != 0:
         raise ValueError("Necesita se par")
     return id
 
+
+#==============Validaciones para Números==================
+# gt (greater than)
+# ge (greater than or equal)
+# lt (less than)
+# le (less than or equal)
+
+
+
+#     Ejemplo validacion en query para numeros
+# @app.get("/items/")
+# async def read_items(q: Annotated[
+#     int | None, Query(gt=3)
+#     ] = None):
+#     results: dict = {"mensaje": "Acceso a get(read_items)"}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+
+# ==================== Metadata ==========================
+# tittle
+# description
+# alias
+# deprecated
+
+
+
+#    Ejemplo con Metadata
 @app.get("/items/")
-async def read_items(q: Annotated[int | None, Query(gt=3)] = None):
+async def read_items(q: Annotated[
+    int | None, Query(gt=3, tittle="Query", description="Lo que se va a buscar")
+    ] = None):
     results: dict = {"mensaje": "Acceso a get(read_items)"}
     if q:
         results.update({"q": q})
