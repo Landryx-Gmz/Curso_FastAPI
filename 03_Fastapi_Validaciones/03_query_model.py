4a787398756cfe446325310ec3f63ca5c2503ef6
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-08-18 17:57:02 +0200

    query parameter con basemodel

---
+++ 03_Fastapi_Validaciones/03_query_model.py
@@ -0,0 +1,14 @@
+from typing import Annotated, Literal
+from fastapi import FastAPI, Query
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+class FilterParams(BaseModel):
+    limit: Annotated[int, Field(gt=0)]
+    offset: int
+    order_by: Literal["created_at", "updated_at"] = "created_at"
+
+@app.get("/items/")
+async def read_items(filster_query: Annotated[FilterParams, Query()]):
+    return {"message": "todo bien!", **filster_query.model_dump()}
