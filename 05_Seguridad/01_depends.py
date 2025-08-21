54ba8f78baca7564cac7cc64a5e6e82616ba09b1
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-08-21 21:14:08 +0200

    ejemplo de dependencia

---
+++ 05_Seguridad/01_depends.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, Depends, HTTPException
+from typing import Annotated
+
+app = FastAPI()
+
+class Logger:
+    def log(self, message: str) -> None:
+        print(f"Logging message: {message}")
+
+# @app.get("/products/{message}")
+# def get_products(message: str):
+#     logger = Logger()
+#     logger.log(message)
+
+# @app.get("/item/{message}")
+# def get_item(message: str):
+#     logger = Logger()
+#     logger.log(message)
+
+def get_logger():# Si este logger cambiase todos los endpoint lo harian tambien asi no se tiene de duplicar codigo
+    return Logger()
+
+logger_dependency = Annotated[Logger, Depends(get_logger)]
+
+
+@app.get("/item/{message}")
+def get_item(message: str, logger: logger_dependency):
+    logger.log(message)
+    return message
+
+@app.get("/products/{message}")
+def get_products(message: str,logger: logger_dependency):
+    logger.log(message)
+    return message
