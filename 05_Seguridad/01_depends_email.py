b7219ae91b63a46f7b4665bc56e1f0c244e4ca83
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-08-25 19:54:52 +0200

    ejemplo autenticacion Query parameters

--- 05_Seguridad/01_depends_email.py
+++ 05_Seguridad/01_depends_email.py
@@ -17,20 +17,27 @@ app = FastAPI()
 
 #========Ejemplo de servicion de autenticacion con dependecia========
 
-class AuthService:
-    def authenticate(self, token: str):
-        if token == "valid_token":
-            return True
-        else:
-            raise HTTPException(status_code= 401, detail="Unauthorized")
-def get_auth_service():
-    return AuthService
+# class AuthService:
+#     def authenticate(self, token: str):
+#         if token == "valid_token":
+#             return True
+#         else:
+#             raise HTTPException(status_code= 401, detail="Unauthorized")
+# def get_auth_service():
+#     return AuthService
 
-auth_service_dependency = Annotated[AuthService, Depends(get_auth_service)]
+# auth_service_dependency = Annotated[AuthService, Depends(get_auth_service)]
 
-@app.get("/secure-data"/)
-def get_secure_data(token: str,auth_service: auth_service_dependency):
-    if auth_service.authenticate(token):
-        return{"Data": "Data segura!"}
+# @app.get("/secure-data/")
+# def get_secure_data(token: str,auth_service: auth_service_dependency):
+#     if auth_service.authenticate(token):
+#         return{"Data": "Data segura!"}
 
-        
+#===========Ejemplo Query Paramaters===================
+
+def common_parameters(q: str |None = None, skip: int =0, limit: int =100):
+    return{"q": q, "skip": skip, "limit":limit}
+
+@app.get("/items/")
+def get_items(commons: Annotated[dict, Depends(common_parameters)]):
+    return commons
