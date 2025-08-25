10e4dc01414569d5f2ec06ce9066d113a52956e2
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-08-25 19:44:50 +0200

    ejemplo email servicio de autenticacion

--- 05_Seguridad/01_depends_email.py
+++ 05_Seguridad/01_depends_email.py
@@ -3,11 +3,34 @@ from typing import Annotated
 
 app = FastAPI()
 
-class EmailService:
-    def send_email(self, recipinet:str, message:str):
-        print(f"Sendig email to {recipinet}: {message}")
+# class EmailService:
+#     def send_email(self, recipinet:str, message:str):
+#         print(f"Sendig email to {recipinet}: {message}")
 
-def get_email_service():
-    return EmailService()
+# def get_email_service():
+#     return EmailService()
 
-email_service_dependency = Annotated[EmailService, Depends(get_email_service)]
+# email_service_dependency = Annotated[EmailService, Depends(get_email_service)]
+
+# def send_email(recipient: str, message: str, email_service: email_service_dependency):
+#     email_service.send_email(recipient, message)
+
+#========Ejemplo de servicion de autenticacion con dependecia========
+
+class AuthService:
+    def authenticate(self, token: str):
+        if token == "valid_token":
+            return True
+        else:
+            raise HTTPException(status_code= 401, detail="Unauthorized")
+def get_auth_service():
+    return AuthService
+
+auth_service_dependency = Annotated[AuthService, Depends(get_auth_service)]
+
+@app.get("/secure-data"/)
+def get_secure_data(token: str,auth_service: auth_service_dependency):
+    if auth_service.authenticate(token):
+        return{"Data": "Data segura!"}
+
+        
