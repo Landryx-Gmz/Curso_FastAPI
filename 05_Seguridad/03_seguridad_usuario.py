9222705095a237a9afeaf335b671c705fe8626a5
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-08-26 16:54:08 +0200

    ejemplo seguridad usuario2

--- 05_Seguridad/03_seguridad_usuario.py
+++ 05_Seguridad/03_seguridad_usuario.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from typing import Annotated
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -48,6 +48,19 @@ def fake_decode_token(token: str) -> UserInDB | None:
 async def get_current_user(token: Annotated[str,Depends(oauth2_scheme)]):
     user = fake_decode_token(token)
     return user
+
+
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrec username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if hashed_password != user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    # Retornar "token falso" (username)
+    return {"access_token": user.username, "token_type": "bearer"}
 
 @app.get("/users/me")
 async def read_users_me(current_user:Annotated[User,Depends(get_current_user)]):
