b841add1dd80ad35495459819bbabba2f40a711e
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-08-30 18:20:25 +0200

    capas de ejecucion de middleware

--- 06_Middleware_CORS/01_middleware.py
+++ 06_Middleware_CORS/01_middleware.py
@@ -4,16 +4,29 @@ from typing import Callable
 
 app = FastAPI()
 
+# @app.middleware("http")
+# async def add_process_time_header(request: Request, call_next: Callable):
+#     strart_time = time.perf_counter()    
+#     response = await call_next(request)
+#     process_time = time.perf_counter() - strart_time
+#     print(process_time)
+#     response.headers["X-Process_Time"] = str(process_time)
+#     return response
+
 @app.middleware("http")
-async def add_process_time_header(request: Request, call_next: Callable):
-    strart_time = time.perf_counter()    
+async def first_middleware(request: Request, call_next: Callable):
+    print("First, before request")
     response = await call_next(request)
-    process_time = time.perf_counter() - strart_time
-    print(process_time)
-    response.headers["X-Process_Time"] = str(process_time)
+    print("First, after request")
     return response
-
-
+    
+    
+@app.middleware("http")
+async def second_middleware(request: Request, call_next: Callable):
+    print("Second, before request")
+    response = await call_next(request)
+    print("Second, after request")
+    return response
 
 
 
