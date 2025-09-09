4eada61dda2404b27d64817feffcdb7ec42b07cf
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-09-09 20:13:54 +0200

    test avanzado fixtures

--- 09_Testing/test_avanzado.py
+++ 09_Testing/test_avanzado.py
@@ -1,3 +1,18 @@
 from fastapi.testclient import TestClient
 import pytest
 from avanzado import app, MockDatabase, get_db
+
+client = TestClient(app)
+
+# Fixtures (Forma limpia estructurada de repetir codigo o utilizar los valores ejmeplo: BD codigos, http, Item etc)
+@pytest.fixture
+def test_db():
+    return MockDatabase
+
+@pytest.fixture 
+def client_with_db(test_db):
+    async def override_get_db():
+        yield test_db
+
+    app.dependency_overrides[get_db] = override_get_db
+    return TestClient(app)
