bb9a2a37b72d12f9d0ef54a1f854d68e4e38d640
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-09-09 20:20:57 +0200

    test avanzado fixtures fin

--- 09_Testing/test_avanzado.py
+++ 09_Testing/test_avanzado.py
@@ -16,3 +16,20 @@ def client_with_db(test_db):
 
     app.dependency_overrides[get_db] = override_get_db
     return TestClient(app)
+
+@pytest.fixture
+def sample_item():
+    return{
+        "id" : "test1",
+        "name" : "Test Item",
+        "price" : 12.99
+    }
+def test_create_and_read_item(client_with_db, sample_item):
+    client = client_with_db
+    response = client.post("/items/", json=sample_item)
+    assert response.status_code==200
+
+    response = client.get(f"/items/{sample_item['id']}")
+    assert response.status_code == 200
+    assert response.json() == sample_item
+
